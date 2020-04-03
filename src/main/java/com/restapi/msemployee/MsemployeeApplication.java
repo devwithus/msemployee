b0db20d0d83152e6a02706e1bfa4397d77415d35
Azhwani <azhwani@devwithus.com>
2020-04-04 01:11:03 +0200

    First commit

---
+++ src/main/java/com/restapi/msemployee/MsemployeeApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.msemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsemployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsemployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/msemployee/MsemployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.msemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsemployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
